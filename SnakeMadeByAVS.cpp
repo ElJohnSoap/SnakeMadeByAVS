dd2101620f32903a4c48d0ad85d5f52dd3b9c641
Vadim Artemov <93925370+ElJohnSoap@users.noreply.github.com>
2022-02-03 20:44:18 +0300

    tail

--- SnakeMadeByAVS.cpp
+++ SnakeMadeByAVS.cpp
@@ -2,77 +2,154 @@
 #include <iostream>
 #include <conio.h>
 #include <Windows.h>
-
 using namespace std;
+
 const int sizeX = 40, sizeY = 20;
-int speed, appleX, appleY, snakeX, snakeY, score;
-char ArrFieldSize[sizeX][sizeY];
+int speed, appleX, appleY, snakeX, snakeY, score, prevX, prevY;
+bool endGame;
+int tailArr[sizeX],tailArr2[sizeY];
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
 
-
-void ArrField()
-{
-    //system("cls");
-    for (int i = 0; i < sizeX; i++)
-    {
-        for (int j = 0; i < sizeY; j++)
-        {
-            cout << ArrFieldSize[i][j];
-        }
-        cout << endl;
-    }
-}
-void GameField();
-
-
-
-
-int main()
-{
-	appleX = rand() % sizeX;
-	appleY = rand() % sizeY;
+void Setup() {
+	srand(time(0));
+	endGame = true;
+	//dir = STOP;
 	snakeX = sizeX / 2;
 	snakeY = sizeY / 2;
-    setlocale(0, "");
-    GameField();
+	appleX = rand() % sizeX;
+	appleY = rand() % sizeY;
+	score = 0;
+	
 }
+
+
 void GameField()
 {
-
-	int aplleX = rand() % sizeX;
-	int aplleY = rand() % sizeY;
-	Sleep(100);    //speed of game
+	Sleep(400);    //speed of game
 	system("cls");
 	for (int i = 0; i < sizeX + 1; i++)
 		cout << "#";
 	cout << endl;
-
 	for (int i = 0; i < sizeY; i++) {
 		for (int j = 0; j <= sizeX; j++) {
-			if (j == 0 || j == sizeX - 1)
-				cout << "#";
-
-			if (i == snakeY && j == snakeX)
+			if (j == 0 || j == sizeX)
 				cout << "0";
-			else if (i == appleY && j == appleX)
-				cout << "F";
 
-			/*else {
-				printTail = false;
-				for (int k = 0; k < nTail; k++) {
-					if (tailX[k] == j && tailY[k] == i) {
-						printTail = true;
-						cout << "o";
-					}
-				}
-				if (!printTail)
-					cout << " ";
-			}*/
+			else if (i == snakeY && j == snakeX)
+				cout << "S";
+			else if (i == appleY && j == appleX)
+				cout << "@";
+
+			else if (prevX == j && prevY == i)
+				cout << "o";
 			else cout << " ";
+			
 		}
 		cout << endl;
 	}
 	for (int i = 0; i < sizeX + 1; i++)
 		cout << "#";
 	cout << endl;
-	//cout << "Score:" << score << endl;
+	cout << "Score:" << score << endl;
+}
+
+void Input() {
+	if (_kbhit()) {
+		switch (_getch())
+		{
+		case 'a':
+			dir = LEFT;
+			break;
+		case 'd':
+			dir = RIGHT;
+			break;
+		case 'w':
+			dir = UP;
+			break;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'x':
+			endGame = false;
+			break;
+		case 'A':
+			dir = LEFT;
+			break;
+		case 'D':
+			dir = RIGHT;
+			break;
+		case 'W':
+			dir = UP;
+			break;
+		case 'S':
+			dir = DOWN;
+			break;
+		case 'X':
+			endGame = false;
+			break;
+		}
+	}
+}
+
+void Logica()
+{
+	prevX = snakeX;
+	prevY = snakeY;
+	//int prev2X, prev2Y;
+	//tailArr[0] = snakeX;
+	//tailArr2[0] = snakeY;
+	switch (dir)
+	{
+	case LEFT:
+		snakeX--;
+		break;
+
+	case RIGHT:
+		snakeX++;
+		break;
+
+	case UP:
+		snakeY--;
+		break;
+
+	case DOWN:
+		snakeY++;
+		break;
+
+	}
+	if (snakeX >= sizeX)
+		snakeX = 0;
+	else if (snakeX < 0)
+		snakeX = sizeX - 1;
+
+	if (snakeY >= sizeY)
+		snakeY = 0;
+	else if (snakeY < 0)
+		snakeY = sizeY - 1;
+
+	if (snakeX == appleX && snakeY == appleY)
+	{
+		score += 10;
+		appleX = rand() % sizeX;
+		appleY = rand() % sizeY;
+	}
+}
+
+
+
+
+
+int main()
+{
+	setlocale(0, "");
+	
+	Setup();
+	while (endGame)
+	{
+		GameField();
+		Input();
+		Logica();
+	}
+	return 0;
 }
